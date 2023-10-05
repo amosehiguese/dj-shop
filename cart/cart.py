1ea4e8f9900f39f86844fadb4079bd388921c360
amosehiguese <amosehiguese@gmail.com>
2023-10-06 00:03:52 +0100

    Complete add, update, and del functionality for cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,13 +1,13 @@
 from decimal import Decimal
 from django.conf import settings
-import shop_app.models import Product
+from shop_app.models import Product
 
 class Cart:
     def __init__(self, request):
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            cart = self.session[settings.CART_SESSION_ID]
+            cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
@@ -15,7 +15,7 @@ class Cart:
         Add a product to the cart or update its quantity
         """
         product_id = str(product.id)
-        if product_id not is self.cart:
+        if product_id not in self.cart:
             self.cart[product_id] = {
                 'quantity': 0,
                 'price': str(product.price)
@@ -41,8 +41,8 @@ class Cart:
         Iterate over the items in the cart and get the products
         from the database.
         """
-        products_ids = self.cart.keys()
-        products = Product.object.filter(id__in=product_ids)
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
         for product in products:
             cart[str(product.id)]['product'] = product
